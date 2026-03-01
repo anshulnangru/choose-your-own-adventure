ca55b55dd2b08254fc817b5d27cc494867bc24ad
Anshul <anshulnangru56@gmail.com>
2026-03-01 20:43:41 +0530

    StoryLoader Component done

--- Frontend/src/components/StoryLoader.jsx
+++ Frontend/src/components/StoryLoader.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react";
 import {useParams, useNavigate} from "react-router-dom";
 import axios from "axios";
 import LoadingStatus from "./LoadingStatus.jsx";
+import loadingStatus from "./LoadingStatus.jsx";
 
 const API_BASE_URL="/api"
 
@@ -13,6 +14,10 @@ function StoryLoader()
     const [loading,setLoading]=useState(true);
     const[error,setError]=useState(null);
 
+    useEffect(() => {
+        loadStory(id)
+    }, [id]);
+
     const loadStory = async (storyId) =>{
         setLoading(true);
         setError(null)
@@ -20,6 +25,44 @@ function StoryLoader()
         try{
             const response= await axios.get(`${API_BASE_URL}/stories/{storyId}/complete`)
             setStory(response.Data)
+            setLoading(false)
+        }
+        catch (err)
+        {
+            if(err.response?.status === 404)
+            {
+                setError("Story not found")
+            }
+            else
+            {
+                setError("Failed to load story")
+            }
+        }
+         finally{
+                setLoading(false)
         }
     }
+    const createNewStory = () =>{
+        navigate("/")
+    }
+    if (loading){
+        return <loadingStatus theme={"story"}/>
+    }
+
+    if (error){
+        return <div className="story-loader">
+
+            <div className="error-message">
+                <h2>Story Not Found</h2>
+                <p>{error}</p>
+                <button onClick={createNewStory} Go to story generator></button>
+            </div>
+
+        </div>
+    }
+
+    if (story) {
+        return <div className="story-loader"></div>
+    }
 }
+export default StoryLoader;
